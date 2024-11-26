0b73c959b28e322ad20c68bf918bc1120d32b1de
KevinWMcGowan <kev.w.mcgowan@gmail.com>
2024-11-26 15:56:46 -0800

    Create predicting_HS_grad_analysis.R

---
+++ scripts/predicting_HS_grad_analysis.R
@@ -0,0 +1,60 @@
+# Predicting High School Graduation Analysis
+
+# This R Script uses Paulo Cortez's Student Performance Dataset (2008) to create a machine learning algorithm.
+  # Specifically, this report builds and compares two different classification models and uses as the primary 
+  # measure of accuracy. 
+
+
+# Requirements:
+# - Ensure necessary libraries are installed (see library loading section).
+# - The script will download the MovieLens dataset if not already present. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#will likely need boot strapping for small dataset...
+
+
+
+
+
+
+# References 
+# Cortez, P. (2008). Student Performance [Dataset]. UCI Machine Learning Repository. https://doi.org/10.24432/C5TG7T.
