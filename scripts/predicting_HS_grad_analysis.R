2aac45c22885c469ed0f8209a6898d6a3a9bd6f4
KevinWMcGowan <kev.w.mcgowan@gmail.com>
2024-11-26 16:21:27 -0800

    added datasets

--- scripts/predicting_HS_grad_analysis.R
+++ scripts/predicting_HS_grad_analysis.R
@@ -1,13 +1,16 @@
-# Predicting High School Graduation Analysis
+# Predicting High School Core Course Performance Analysis
 
 # This R Script uses Paulo Cortez's Student Performance Dataset (2008) to create a machine learning algorithm.
-  # Specifically, this report builds and compares two different classification models and uses as the primary 
+  # Specifically, this report builds and compares two different classification models that predict pass or fail for
+  # in Portuguese (equivalent of English class in US) or mathematics. RMSE is used as the primary 
   # measure of accuracy. 
 
 
 # Requirements:
 # - Ensure necessary libraries are installed (see library loading section).
-# - The script will download the MovieLens dataset if not already present. 
+# - The script will download the Student Perforamnce dataset (2008) if not already present. 
+
+
 
 
 
